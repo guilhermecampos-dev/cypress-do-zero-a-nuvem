39a3c3636e2cf9b815738feca70c914dda607755
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2025-04-03 23:19:14 -0300

    Outra forma de usar os comandos customizados

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -13,7 +13,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Campos')
     cy.get('#email').type('guimcampos@gmail.com')
     cy.get('#open-text-area').type(longText, {delay:0})
-    cy.get('button[type="submit"]').click()
+    cy.contain('button','Enviar').click()
 
 
     cy.get('.success').should('be.visible')
@@ -25,7 +25,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Campos')
     cy.get('#email').type('guimcampos@gmail,com')
     cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contain('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
   })
@@ -42,7 +42,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('guimcampos@gmail,com')
     cy.get('#phone-checkbox').click()
     cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contain('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
 
@@ -74,7 +74,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
 it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-  cy.get('button[type="submit"]').click()
+  cy.contain('button', 'Enviar').click()
 
   cy.get('.error').should('be.visible')
 })
