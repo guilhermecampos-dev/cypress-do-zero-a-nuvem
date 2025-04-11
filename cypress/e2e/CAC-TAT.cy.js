4ae3f4b3783a193587faa4782c92525e1f89ee0f
Guilherme Campos <157999298+guilhermecampos-dev@users.noreply.github.com>
2025-04-11 18:13:52 -0300

    configuração de tela para mobile e teste hadless

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -162,13 +162,13 @@ it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', 
 })
 })
 
-it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
   cy.contains('a','Política de Privacidade')
     .should('have.attr', 'href', 'privacy.html')
     .and('have.attr', 'target', '_blank')
 })
 
-it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
   cy.contains('a', 'Política de Privacidade')
     .invoke('removeAttr', 'target')
     .click()
